308bca67e0fc69fad65f3bff86edf9625a86712f
Madhu <siddumadhu1296@gmail.com>
2020-04-16 13:39:41 +0530

    Initial commit

---
+++ app/src/main/java/com/example/siddu/whatsup/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.siddu.whatsup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+    private Toolbar mToolbar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       mToolbar = (Toolbar)findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Whatsup");
+    }
+}
+
+
+
